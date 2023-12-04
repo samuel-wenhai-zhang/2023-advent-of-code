c21e88eb21200fe1da33db31c55b6d3596fa1275
Boltcrasher <Boltcrasher@gmail.com>
2023-12-04 13:34:32 -0600

    Day02

---
+++ Day02/Day02.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Day02 {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("day02.dat"));
+        
+    }
+}
