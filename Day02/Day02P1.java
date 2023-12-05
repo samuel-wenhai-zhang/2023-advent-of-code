dfc04cd3c0ae546fe1af39e2b7e1e41744939fd8
Boltcrasher <Boltcrasher@gmail.com>
2023-12-05 12:43:51 -0600

    Day02 Done

---
+++ Day02/Day02P1.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Day02P1 {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("day02.dat"));
+        int sum = 0;
+
+        while (in.hasNextLine()) {
+            String[] line = in.nextLine().split("[:;]");
+            int id = Integer.parseInt(line[0].substring(line[0].indexOf(" ") + 1));
+            Map<String, Integer> colors = new HashMap<String, Integer>();
+            colors.put("red", 0);
+            colors.put("green", 0);
+            colors.put("blue", 0);
+            for (int i = 1; i < line.length; i++) {
+                String[] cubes = line[i].split(",");
+                for (String cube : cubes) {
+                    try (Scanner reader = new Scanner(cube)) {
+                        int num = reader.nextInt();
+                        String color = reader.next();
+                        if (num > colors.get(color)) {
+                            colors.put(color, num);
+                        }
+                    }
+                }
+            }
+            if (colors.get("red") <= 12 && colors.get("green") <= 13 && colors.get("blue") <= 14) {
+                sum += id;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
--- Day02/Day02P2.java
+++ Day02/Day02P2.java
@@ -1,33 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-public class Day02 {
+public class Day02P2 {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new File("day02.dat"));
         int sum = 0;
 
         while (in.hasNextLine()) {
             String[] line = in.nextLine().split("[:;]");
-            int id = Integer.parseInt(line[0].substring(line[0].indexOf(" ") + 1));
             Map<String, Integer> colors = new HashMap<String, Integer>();
-            colors.put("red", 0);
-            colors.put("green", 0);
-            colors.put("blue", 0);
             for (int i = 1; i < line.length; i++) {
                 String[] cubes = line[i].split(",");
                 for (String cube : cubes) {
                     try (Scanner reader = new Scanner(cube)) {
                         int num = reader.nextInt();
                         String color = reader.next();
+                        if (!colors.containsKey(color)) {
+                            colors.put(color, num);
+                        }
                         if (num > colors.get(color)) {
                             colors.put(color, num);
                         }
                     }
                 }
             }
-            if (colors.get("red") <= 12 && colors.get("green") <= 13 && colors.get("blue") <= 14) {
-                sum += id;
+            int product = 1; // what if there's 0 cubes
+            if (colors.containsKey("red")) {
+                product *= colors.get("red");
             }
+            if (colors.containsKey("green")) {
+                product *= colors.get("green");
+            }
+            if (colors.containsKey("blue")) {
+                product *= colors.get("blue");
+            }
+            sum += product;
 
         }
         System.out.println(sum);
